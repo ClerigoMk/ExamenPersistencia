50d18513fc1b6280bf0bbc39b223148d55ea00cc
Mark Espinoza <111921131+ClerigoMk@users.noreply.github.com>
2024-10-22 18:32:03 -0500

    Delete api-rest-g7 directory

--- api-rest-g7/src/main/java/com/codigo/api_rest_g7/ejercicio1/service/GreetingService.java
+++
@@ -1,5 +0,0 @@
-package com.codigo.api_rest_g7.ejercicio1.service;
-
-public interface GreetingService {
-    String greet();
-}
--- api-rest-g7/src/main/java/com/codigo/api_rest_g7/ejercicio7/service/CalculatorService.java
+++
@@ -1,5 +0,0 @@
-package com.codigo.api_rest_g7.ejercicio7.service;
-
-public interface CalculatorService {
-    double calculate(double a, double b);
-}
